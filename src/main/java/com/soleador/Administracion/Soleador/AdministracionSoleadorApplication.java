0eec2fac20b671fbcefb0d60aeb5d33125a8cd28
Lisandro <lisandroscofano@gmail.com>
2020-03-03 16:57:24 -0300

    First Commit

---
+++ src/main/java/com/soleador/Administracion/Soleador/AdministracionSoleadorApplication.java
@@ -0,0 +1,13 @@
+package com.soleador.Administracion.Soleador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionSoleadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionSoleadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soleador/Administracion/Soleador/AdministracionSoleadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soleador.Administracion.Soleador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionSoleadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
